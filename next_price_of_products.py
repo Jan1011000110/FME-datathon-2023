55687ebc0119f7572c10293e4cd3ac2aac4920ed
Jan <janmatasc@gmail.com>
2023-11-11 20:23:30 +0100

    a

--- next_price_of_products.py
+++ next_price_of_products.py
@@ -1,29 +1,30 @@
 from read_data import read_data
 from separate_products import separate_products
 from separete_years import separete_years
-from create_graph import generate_graph
 
 def get_predicted_price_of_products():
 
-    def predict_price(product, years):
-        keys = [*years.keys()]
-        keys.sort()
-        keys.reverse()
+    def predict_price(years):
+        years.sort()
+        years.reverse()
 
-        average_price_by_year = []
+        average_price_by_year = {}
 
-        for year in keys:
-            lst = years[year]
+        for year, list in years:
             total_price = 0
             total_units = 0
 
-            for i in range(len(lst)):
+            for i in range(len(list)):
                 row = data[i]
                 total_price += row['PRECIO']
                 total_units += row['UNIDADESCONSUMOCONTENIDAS']
             
             average = total_price/total_units
-            average_price_by_year.append(average)
+
+            if not year in year:
+                average_price_by_year[year] = [average]
+            else:
+                average_price_by_year[year].append(average)
 
         predicted_price = average_price_by_year[0]
         coef = 1/2
@@ -32,18 +33,8 @@ def get_predicted_price_of_products():
             average1 = average_price_by_year[i]
             average2 = average_price_by_year[i+1]
 
-            if (i == len(average_price_by_year)-2):
-                coef *= 2
             predicted_price += coef*(average1-average2)
             coef *= 1/2
-
-        average_price_by_year.reverse()
-        average_price_by_year.append(predicted_price)
-        keys.reverse()
-        keys.append("24")
-        print(keys)
-        print(average_price_by_year)
-        generate_graph(keys, average_price_by_year, product)
 
         return predicted_price
 
@@ -68,7 +59,7 @@ def get_predicted_price_of_products():
         years = separete_years(product_data)    
         
         # Predicted
-        predicted_price = predict_price(product, years)
+        predicted_price = predict_price(years)
 
         if not product in predicted_price_by_product:
             predicted_price_by_product[product] = [predicted_price]
@@ -77,5 +68,3 @@ def get_predicted_price_of_products():
     
     return predicted_price_by_product
 
-
-get_predicted_price_of_products()
