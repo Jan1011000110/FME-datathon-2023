d83e905e6de069b14eafb3801ae2f619de3b2149
Sergio <sergiodominguezalonso2005@gmail.com>
2023-11-11 13:33:34 +0100

    recuperar read data

--- read_data.py
+++ read_data.py
@@ -1,1 +1,10 @@
-import pandas as pd 
+
+import pandas as pd
+
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.expand_frame_repr', False)
+
+def read_data():
+    return pd.read_excel('consumo_material_clean.xlsx', index_col=0)  
+
+print(read_data())
