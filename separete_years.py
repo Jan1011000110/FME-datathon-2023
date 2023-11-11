bb8728b760e6465e4f0498f9dedfa1a81b6a4ff1
Jan <96077201+Jan1011000110@users.noreply.github.com>
2023-11-11 15:20:37 +0100

    Add files via upload

---
+++ separete_years.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from read_data import read_data
+
+def separete_years(data):
+    years = {}
+    for i in range(len(data)):
+        row = data[i]
+        date = row['FECHAPEDIDO']
+        year = date[-2:]
+        print(year)
+        if not year in years:
+            years[year] = [row]
+        else:
+            years[year].append(row)
+
+    return years
