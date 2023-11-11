a1d3e64643424234d123428595e8f6cdca88f7d0
Sergio <sergiodominguezalonso2005@gmail.com>
2023-11-11 23:06:47 +0100

    a

--- calculate_expected_price.py
+++ calculate_expected_price.py
@@ -11,9 +11,13 @@ for product in group_amounts:
     total_amounts[product] = add(group_amounts[product])
 
 total_price = 0
+product_prices = {}
 
 for product in total_amounts:
     product_price = total_amounts[product] * prices[product]
+    product_prices[product] = product_price
     total_price += product_price
 
 print("Total price:", total_price)
+print("Product prices:")
+print(product_prices)
