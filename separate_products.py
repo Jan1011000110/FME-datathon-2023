e879756fe9cf549ab2a25da254a435f301771abf
Sergio <sergiodominguezalonso2005@gmail.com>
2023-11-11 15:03:41 +0100

    a

--- separate_products.py
+++ separate_products.py
@@ -1,3 +1,31 @@
+import pandas as pd
+from read_data import read_data
+
 
 def separate_products(data):
-    
+    products = {}
+
+    for i in range(len(data)):
+        row = data[i]
+
+        product = row['CODIGO']
+
+        if not product in products:
+            products[product] = [row]
+        else:
+            products[product].append(row)
+
+
+data = read_data()
+
+data = [data.iloc[i] for i in range(len(data))]
+
+products = separate_products(data)
+
+keys = products.keys()
+
+keys.sort()
+
+
+
+
