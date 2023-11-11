dadffe56c2b720cd93a9b1360000152dd51626af
Sergio <sergiodominguezalonso2005@gmail.com>
2023-11-11 13:21:33 +0100

    print data

--- read_data.py
+++ read_data.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 
-
 pd.set_option('display.max_columns', 1000)
+pd.set_option('display.expand_frame_repr', False)
 
-data = pd.read_excel('consumo_material_clean.xlsx', index_col=0)  
+def read_data():
+    return pd.read_excel('consumo_material_clean.xlsx', index_col=0)  
 
-print(data)
-
+print(read_data())
---
+++ separate_hospitals.py
@@ -0,0 +1,10 @@
+
+from read_data import read_data
+
+
+data = read_data()
+
+hospitals = {}
+for row in data:
+    print(row)
+    break
