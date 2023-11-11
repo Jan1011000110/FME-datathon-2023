ed27733537c84261c2248a4e17e2a988dc1ffbce
Jan <janmatasc@gmail.com>
2023-11-11 23:04:11 +0100

    a

--- next_buy_of_products.py
+++ next_buy_of_products.py
@@ -1,4 +1,35 @@
-
+NUMBER_OF_BUYS = 6
 
 def get_predicted_buy_of_products(data):
-    pass
+    
+    def predict_buy(years, buy):
+        buys_by_year = []
+
+        for lst_buy in years:
+            buys_by_year.append(lst_buy[buy])
+        
+        predicted_buy = buys_by_year[0]
+        coef = 1/2
+
+        for i in range(len(buys_by_year)-1):
+            buy1 = buys_by_year[i]
+            buy2 = buys_by_year[i+1]
+
+            predicted_buy += coef*(buy1-buy2)
+            coef *= 1/2
+        
+        return predicted_buy
+
+    
+    predicted_buy_by_product = {}
+
+    for buy in range(NUMBER_OF_BUYS):
+        for product, years in data.items():
+            predicted_buy = predict_buy(years, buy)
+
+            if not product in predicted_buy_by_product:
+                predicted_buy_by_product[product] = [predicted_buy]
+            else:
+                predicted_buy_by_product[product].append(predicted_buy)
+    
+    return predicted_buy_by_product
